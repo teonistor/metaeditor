0383ae2ac8bd30ad134f9287d60b9a8e60bad49c
Teo G Nistor <teo.g.nistor@gmail.com>
2019-01-10 22:25:17 +0000

    Begining of camera recentering event logic **Notes**
    From here we must divide focus:
     - For builder (no game view), minimal inspector tools to save mesh data to a file and later load&recreate (or find out how to do the Unity way but beware we have single triangles in separate objects)
     - For gamification, camera action, mouse interaction, PURPOSE, multiple objects, rulers, lighting... (but find a purpose that doesn't require reinventing the whole inspector)
    
    Past observation note:
     - Attempting to draw triangle on both sides results in black faces (shader choice problem?)

---
+++ Assets/Assets/Script/FocusOnObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocusOnObject : MonoBehaviour {
+
+	void Start () {
+        Meshable.OnCentreChange += OnCentreChange;
+	}
+	
+	void Update () {
+		
+	}
+
+    void OnDisable () {
+        Meshable.OnCentreChange -= OnCentreChange;
+    }
+
+    void OnCentreChange(string objName, Vector3 newCentre, float distance) {
+        print("Centre has become " + newCentre);
+    }
+
+    //IEnumerator Refocus () {
+    //    WaitForSeconds wait = new WaitForSeconds
+    //}
+}
---
+++ Assets/Assets/Script/Meshable.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+
+public class Meshable : MonoBehaviour {
+    internal const string objName = "Default";
+
+    internal static event Action<string, Vector3, float> OnCentreChange;
+
+    // Initially a tetrahedron
+    [SerializeField] Vertex a, b, c, d;
+
+    Vector3 centre;
+
+    IEnumerator Start () {
+        Face[] faces = GetComponentsInChildren<Face>();
+        if (faces.Length != 4) {
+            Debug.LogErrorFormat("Cannot construct tetrahedron with {0} faces", faces.Length);
+            yield break;
+        }
+
+        faces[0].Init(a, b, c);
+        faces[1].Init(a, c, d);
+        faces[2].Init(a, d, b);
+        faces[3].Init(b, d, c);
+
+        // More seldom update the centre point
+        WaitForSeconds wait = new WaitForSeconds(0.5f);
+        while (true) {
+            Vector3 newCentre = Vector3.zero;
+            Vertex[] vertices = GetComponentsInChildren<Vertex>();
+            foreach (Vertex v in vertices) {
+                newCentre += v.transform.position;
+            }
+            newCentre /= vertices.Length;
+
+            if (!newCentre.Equals(centre)) {
+                centre = newCentre;
+                if (OnCentreChange != null) {
+                    float dist = 0f;
+                    foreach (Vertex v in vertices) {
+                        dist = Mathf.Max(dist, Vector3.Distance(centre, v.transform.position));
+                    }
+
+                    OnCentreChange(objName, centre, dist);
+                }
+            }
+
+            yield return wait;
+        }
+    }
+
+	void Update () {
+		
+	}
+}
